4cf242e2feb95351f8f1bf60b72b44bc447471b3
Yuriy Sidorov <vipyurist@gmail.com>
2021-01-06 15:13:58 +0300

    Updates

---
+++ precache-manifest.33499ab9d1a23037b700545b94a139aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d061e6b8171be3887a7d67bd592a847e",
+    "url": "/diagram/index.html"
+  },
+  {
+    "revision": "bfdc5e6e05e1594944cd",
+    "url": "/diagram/static/css/main.abfe1074.chunk.css"
+  },
+  {
+    "revision": "976b97abf97d7494b476",
+    "url": "/diagram/static/js/2.771db56a.chunk.js"
+  },
+  {
+    "revision": "e64e5ee3c3be090c32ebdbf926b30920",
+    "url": "/diagram/static/js/2.771db56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfdc5e6e05e1594944cd",
+    "url": "/diagram/static/js/main.1ee27c5a.chunk.js"
+  },
+  {
+    "revision": "3655fb5b6c7962536c78",
+    "url": "/diagram/static/js/runtime-main.a8e88486.js"
+  }
+]);
